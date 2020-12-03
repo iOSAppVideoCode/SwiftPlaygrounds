816e3396c7b4674eeb8cef2433f97d9908dcf213
David Fisher <fisherds@rose-hulman.edu>
2020-12-03 16:24:37 -0500

    basics and options part 1

--- miniExamples/02_Optionals.playground/contents.swift
+++ miniExamples/02_Optionals.playground/contents.swift
@@ -6,27 +6,29 @@ let dave = "Dave"
 
 // Part 1.
 // Optionals
+var optionalFloat: Float?
+var requiredFloat: Float
 
+optionalFloat = 5.0
+requiredFloat = 5.0
 
-
-
+print("optionalFloat = \(optionalFloat)")
+print("requiredFloat = \(requiredFloat)")
 
 // Optionals with forced unwrapping
+print("optionalFloat = \(optionalFloat!)")
 
+let name = "Dave"
+let number = "7"
+var optionalThatIsNil: Int? = Int(name)
+var requiredThatIsNotNil: Int = Int(number)!
 
-
-
-
-
-
-
+optionalThatIsNil
+requiredThatIsNotNil
 
 
 // Part 2.
 // Views in a Playground + Optional Chaining
-
-
-
 
 
 
