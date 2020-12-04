2a460882e9ebfbc0fea91a489a904ac55192ebee
David Fisher <fisherds@rose-hulman.edu>
2020-12-04 09:01:06 -0500

    collections

--- miniExamples/03_Collections.playground/contents.swift
+++ miniExamples/03_Collections.playground/contents.swift
@@ -2,28 +2,29 @@ import UIKit
 
 // Basics for arrays
 var names = ["Dave", "Kristy", "McKinley", "Keegan", "Bowen", "Neala"]
-
-
-
-
-
-
-
-
-
+names[1]
+names[2] = "Hello"
+names
+names.count
+if names.contains("Dave") {
+  print("Yes, it is present")
+}
 
 // Appending values
-
-
-
-
-
-
-
-
-
-
-
+var ages = [Int]()
+//ages.append(37)
+//ages.append(4)
+//ages.insert(10, at: 0)
+//
+//ages
+//
+//ages += [1, 3, 7]
+ages
+if ages.isEmpty {
+  print("Yes, empty")
+} else {
+  print("Array has \(ages.count) elements")
+}
 
 // So, so, SO.. many things you could do with arrays...
 var randomAges = [Int]()
@@ -33,9 +34,15 @@ for _ in 0..<100 {
 var teenagers = randomAges.filter({
     return $0 > 12 && $0 < 20
 })
-teenagers.sorted(by: {
+teenagers = teenagers.sorted(by: {
     return $0 < $1
 })
+teenagers
+teenagers = teenagers.map({
+  return $0 + 100
+})
+teenagers
+
 
 
 // Dictionaries
@@ -54,3 +61,34 @@ var tuple = ("item1", "item2", "item3", "item4")
 tuple.2
 var namedTuple = (first : "Dave", last : "Fisher")
 namedTuple.last
+namedTuple.1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
